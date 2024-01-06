604fb9e3d6317616109b94687f466d820909a8ac
Xiaodi Fu <fuxiaodi.amy@gmail.com>
2024-01-05 21:19:59 -0500

    comment database local postgresql settings

--- btre/settings.py
+++ btre/settings.py
@@ -79,16 +79,16 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'btredb',
-        'USER': 'postgres',
-        'PASSWORD': 'Fxd1986@',
-        'HOST': 'localhost',
-        # 'PORT':'5432'
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'btredb',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Fxd1986@',
+#         'HOST': 'localhost',
+#         # 'PORT':'5432'
+#     }
+# }
 
 
 # Password validation
